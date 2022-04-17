8c53455f880bf796fec8daeebf999d82c1d413d6
CSim <100964977+C-Sim@users.noreply.github.com>
2022-04-17 19:36:15 +0100

    inital testing and length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,50 @@
+// Global scope
+
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const number = "0123456789";
+
+const special = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+// Prompt user for password length of 8-128 characters
+const userLength = prompt(
+  "How many characters would you like your password to have? Please enter a number of minimum 8 and maximum 128."
+);
+
+const getPasswordLength = () => {
+  return 10;
+};
+
+const getPasswordCriteria = () => {
+  return [lowercase, uppercase, number, special];
+};
+
+const createPassword = () => {
+  return "Cherelle8!";
+};
+
+// - lowercase "abcdefghijklmnopqrstuvwxyz"
+// - uppercase "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// - numeric "0123456789"
+// - special characters " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
 // main function to generate the random password
 const generatePassword = () => {
-  /*
-   * All your code goes here to generate a password
-   */
+  // get password length
+  const passwordLength = getPasswordLength();
+
+  // get password criteria
+  const passwordCriteria = getPasswordCriteria();
+
+  // create password
+  const password = createPassword(passwordLength, passwordCriteria);
+
+  return password;
 };
 
 // Write password to the #password input
