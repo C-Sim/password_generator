fc6544ff158e96a73f9c8c35701c4e46d3b38498
CSim <100964977+C-Sim@users.noreply.github.com>
2022-04-18 22:07:38 +0100

    output array based on selected criteria and alert if none selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,38 @@
 // Declare criteria strings and split into array
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 
-const lowercaseArray = lowercase.split("abcdefghijklmnopqrstuvwxyz");
+const lowercaseArray = lowercase.split("");
 
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const uppercaseArray = uppercase.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const uppercaseArray = uppercase.split("");
 
 const number = "0123456789";
 
-const numberArray = number.split("0123456789");
+const numberArray = number.split("");
 
 const special = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-const specialArray = special.split(" !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~");
+const specialArray = special.split("");
+
+const criteriaArray = [];
 
 // Generate Password button
 const generateBtn = document.querySelector("#generate");
+
+// const isValidInput = (userLength) => {
+//   if (userLength >= 8 && userLength <= 128) {
+//     // Convert userLength to integer and store as getPasswordLength
+//     return true;
+//   } else {
+//     // If invalid, alert user
+//     alert(
+//       "Invalid input. Please use numeric digits to enter a number of minimum 8 and maximum 128."
+//     );
+//   }
+// };
+
+// const isValidLength = isValidInput(userLength);
 
 const getPasswordLength = () => {
   // Prompt user for password length of 8-128 characters and store in variable of userLength
@@ -44,26 +60,46 @@ const getPasswordCriteria = () => {
   const lowercaseRequired = confirm(
     "Would you like to include lowercase letters?"
   );
+  if (lowercaseRequired == true) {
+    criteriaArray.push(lowercaseArray);
+    console.log(criteriaArray);
+  }
 
   const uppercaseRequired = confirm(
     "Would you like to include uppercase letters?"
   );
+  if (uppercaseRequired == true) {
+    criteriaArray.push(uppercaseArray);
+    console.log(criteriaArray);
+  }
 
   const numberRequired = confirm("Would you like to include numbers?");
+  if (numberRequired == true) {
+    criteriaArray.push(numberArray);
+    console.log(criteriaArray);
+  }
 
   const specialRequired = confirm(
     "Would you like to include special characters?"
   );
+  if (specialRequired == true) {
+    criteriaArray.push(specialArray);
+    console.log(criteriaArray);
+  }
+
+  if (criteriaArray > 0) {
+  } else {
+    alert(
+      "Please select at least one criteria to include in your password generation."
+    );
+  }
 };
 
-// const getPasswordCriteria = (lowercase, uppercase, number, special) => {
-//   return [lowercaseArray, uppercase, number, special];
-// };
-
 const createPassword = () => {
-  // if (lowercaseRequired == true) {
-  //   getPasswordCriteria.push(lowercaseArray);
-  // }
+  // get criteria
+  // from criteria get random character
+  // push to result array
+  // convert result array to string
 };
 
 // main function to generate the random password
